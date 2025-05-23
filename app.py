d248ad8b400c866c3ea45094944ef43dab7ada45
Thameem-20 <ahmedthameem20@gmail.com>
2025-05-23 15:49:40 +0400

    updated

--- app.py
+++ app.py
@@ -111,4 +111,5 @@ def admin():
     return "<br>".join(formatted_logs)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
